9bdace1ac771546b03656ae8f9ea3beac9aba182
Weibin Xu <w.xu@namirial.com>
2026-02-16 09:18:12 +0100

    chore(pages): remove unused legacy route files

--- src/app/pages/customers/customers.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CustomersComponent } from './customers.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: CustomersComponent,
-  },
-];
